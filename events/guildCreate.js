2c764a656ad617d4ae548d005a72c29cd3342556
BazookaBotHoster <51861084+BazookaBotHoster@users.noreply.github.com>
2019-06-19 19:36:27 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,32 +1,105 @@
-const fs = require('fs')
-const snekfetch = require('snekfetch')
-const gdata = require("../database/mongoose/GuildData")
+// init ;p
+const time = Date(),
+pak = require('./package.json'),
+discord = require('discord.js'),
+config = require('./json/config.json'),
+profanities = require("./profanities.json"),
+bot = new discord.Client()
+var prefix = process.env.prefix,
+{baselogger} = require('./src/logger.js'),
+result = Math.round(Math.random()),
+updates = ["Work command added.", "MongoDB is now Hulkbot's official database provider."],
+webhookchannelid = "441710517460008960",
+cleverbot = require('cleverbot.io'),
+ms = require('ms'),
+snekfetch = require('snekfetch'),
+cb = new cleverbot("sMNApmkOjMlZRlPZ", "gskxw3JBqEVGIAboBjOnvyTf8awM1MbS")
+// End of init
 
-module.exports = (bot, guild, discord) => {
-  const channel = guild.channels.find(c => c.name == "welcome")
-  const modc = guild.channels.find(c => c.name == "guild-logs")
-  // post new server count
+// The bot's support server invite vvv
+bot.invite = "https://discord.gg/YpR8yW6"
+// No more invite.
 
-  // Owner msg
-  let member = guild.owner
-  let embed = new discord.RichEmbed()
-  .setTitle("Introduction to Bazooka")
-  .addField("Heyo! Thanks for adding me.","Before you start using me, I need to get some things straight. First of all, for some basic info about me, say `b.info`.")
-  .addField("To see a list of commands","Say `b.help`.", true)
-  .addField("If you need to report a bug:","Or you can just use the `b.contact` command, and it will send a message straight to my creator.")
-  .addField("Done!", "Alrighty! Now you can start using all my awesome features in your server!")
-  .setThumbnail(bot.user.avatarURL)
-  .setTimestamp()
-  member.send({embed: embed})
-  
-  const newD = new gdata({
-    guildId: guild.id,
-    prefix: "b.",
-    isPremium: false,
-    welcome: channel.id || null,
-    modlog: modc.id || null,
-    isBL: false
+// Gather commands
+bot.commands = new discord.Collection();
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log(new Error(reason))
+})
+
+require('fs').readdir("./commands/", (err, files) => {
+  console.log("Loading commands...");
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  });
+});
+
+
+bot.on('error', (err) => {
+  console.error(`Error... ${err}`).then(() => {
+    bot.destroy().then(() => {
+      bot.login(process.env.botToken)
+    })
   })
-  newD.save()
-}
+})
+bot.on("guildMemberAdd", (member) => require('./events/guildMemberAdd.js')(bot, member))
+bot.on("guildMemberRemove", (member) => require('./events/guildMemberRemove.js')(bot, member))
+// bot.on("guildBanAdd", (guild, member) => require('./events/BanAdd.js')(bot, guild, member))
+bot.on("guildBanRemove", (guild, member) => require('./events/BanRemove.js')(bot, guild, member))
 
+  
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+  if (message.channel.type == "dm") return;
+
+  let mArray = message.content.split(" ");
+  let args = mArray.slice(1);
+  let loggedcmd = mArray[0].slice(prefix.length)
+
+  let cmd = bot.commands.get(loggedcmd);
+  if (message.author.bot) return;
+
+  if (cmd) {
+      if (config.userblacklist.includes(message.author.id)) return;
+      cmd.run(bot, message, args, discord)
+      console.log(`${message.author.username} used the ${loggedcmd} command.`);
+      baselogger(bot, `**Command Run**\n\n**Command:** ${loggedcmd}\n**User:** ${message.author.tag}\n**Message:** ${message.content}\n**Guild:** ${message.guild.name}\n**Channel:** ${message.channel.name}`);
+  } 
+    if (message.content == "i love you Bazooka") {
+    message.channel.send("oh god, not another one");
+  }
+  
+  if (message.isMentioned("294194506113220608")) {
+    const em = new discord.RichEmbed()
+    .setTitle(`Bazooka Intro`)
+    .setDescription(`Hey! Thanks for inviting me! If you need help type b.help ! If you want info on me type b.info`)
+    .setColor("GREEN")
+    .setTimestamp()
+    console.log('lol')
+    message.channel.send({embed: em})
+  }
+ });
+      
+bot.on("guildCreate", (guild) => {
+  require('./events/guildCreate.js')(bot, guild, discord)
+  baselogger(bot, `**Guild Join**\n\n**Guild:** ${guild.name}\n**Owner:** ${guild.owner.user.username}\n**Large:** ${guild.large}\n**Member Count:** ${guild.memberCount}\n\n**Total Guilds:** ${bot.guilds.array().length}`, guild.iconURL);
+});
+
+bot.on("guildDelete", (guild) => {
+  require('./events/guildDelete.js')(bot, guild, discord)
+  baselogger(bot, `**Guild Leave**\n\n**Guild:** ${guild.name}\n**Owner:** ${guild.owner.user.username}\n**Large:** ${guild.large}\n**Member Count:** ${guild.memberCount}\n\n**Total Guilds:** ${bot.guilds.array().length}`, guild.iconURL);
+});
+
+bot.login(process.env.botToken); 
+
+let upmsg = `Oh yeah, more updates! New updates:\n${updates}`
+  async function senddat(up,msg) {
+    if (up == null) return;
+   await bot.channels.get('441982405985828864').send(msg).then(() => {
+     up.pop(up)
+   })
+   await bot.channels.get('441982440005697539').send(msg).then(() => {
+     up.pop(up)
+   })
+  }
