efb984f43701a9d9e57af7508b21d37e6b16683a
BazookaBotHoster <51861084+BazookaBotHoster@users.noreply.github.com>
2019-06-19 20:50:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -47,43 +47,7 @@ bot.on('error', (err) => {
     })
   })
 })
-bot.on("guildMemberAdd", (member) => require('./events/guildMemberAdd.js')(bot, member))
-bot.on("guildMemberRemove", (member) => require('./events/guildMemberRemove.js')(bot, member))
-// bot.on("guildBanAdd", (guild, member) => require('./events/BanAdd.js')(bot, guild, member))
-bot.on("guildBanRemove", (guild, member) => require('./events/BanRemove.js')(bot, guild, member))
- 
 
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
-  if (message.channel.type == "dm") return;
-
-  let mArray = message.content.split(" ");
-  let args = mArray.slice(1);
-  let loggedcmd = mArray[0].slice(prefix.length)
-
-  let cmd = bot.commands.get(loggedcmd);
-  if (message.author.bot) return;
-
-  if (cmd) {
-      if (config.userblacklist.includes(message.author.id)) return;
-      cmd.run(bot, message, args, discord)
-      console.log(`${message.author.username} used the ${loggedcmd} command.`);
-      baselogger(bot, `**Command Run**\n\n**Command:** ${loggedcmd}\n**User:** ${message.author.tag}\n**Message:** ${message.content}\n**Guild:** ${message.guild.name}\n**Channel:** ${message.channel.name}`);
-  } 
-    if (message.content == "i love you Bazooka") {
-    message.channel.send("oh god, not another one");
-  }
-  
-  if (message.isMentioned("294194506113220608")) {
-    const em = new discord.RichEmbed()
-    .setTitle(`Bazooka Intro`)
-    .setDescription(`Hey! Thanks for inviting me! If you need help type b.help ! If you want info on me type b.info`)
-    .setColor("GREEN")
-    .setTimestamp()
-    console.log('lol')
-    message.channel.send({embed: em})
-  }
-      
 bot.on("guildCreate", (guild) => {
   require('./events/guildCreate.js')(bot, guild, discord)
   baselogger(bot, `**Guild Join**\n\n**Guild:** ${guild.name}\n**Owner:** ${guild.owner.user.username}\n**Large:** ${guild.large}\n**Member Count:** ${guild.memberCount}\n\n**Total Guilds:** ${bot.guilds.array().length}`, guild.iconURL);
