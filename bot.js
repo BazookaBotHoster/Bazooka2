192b6100f2fb032fd3cdff97cfc72f31e5a47754
BazookaBotHoster <51861084+BazookaBotHoster@users.noreply.github.com>
2019-06-19 18:56:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,9 +23,6 @@ bot.invite = "https://discord.gg/YpR8yW6"
 // Gather commands
 bot.commands = new discord.Collection();
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.log(new Error(reason))
-})
 
 require('fs').readdir("./commands/", (err, files) => {
   console.log("Loading commands...");
